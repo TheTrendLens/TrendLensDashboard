787179316908b7e082ec0d718186dad0019c8639
James <jamesc554544@gmail.com>
2024-07-20 13:27:47 +0100

    Test Local API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   backend: {
-    baseURL: "https://api.thetrendlens.co.uk"
+    baseURL: "http://localhost:3000"
   },
   firebaseConfig: {
     apiKey: "AIzaSyAe4OWrwydmxI6pYtI0FpJv_ZXxtXUZ1-4",
