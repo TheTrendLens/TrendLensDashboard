ce0650df8f275a1e6a7101a302efa71616896178
James <jamesc554544@gmail.com>
2025-01-27 11:31:14 +0000

    Fix prod api server url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   backend: {
-    baseURL: "https://api.thetrendlens.co.uk/api"
+    baseURL: "https://api.thetrendlens.co.uk"
   },
   firebaseConfig: {
     apiKey: "AIzaSyAe4OWrwydmxI6pYtI0FpJv_ZXxtXUZ1-4",
