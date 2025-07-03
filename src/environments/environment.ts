58cc1a44a4b1da7bf6aa35528cb5a7847658d58d
James <jamesc554544@gmail.com>
2025-07-03 21:43:05 +0100

    Environment Vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ enableProdMode();
 export const environment = {
   production: true,
   backend: {
-    baseURL: "https://api.thetrendlens.co.uk"
+    baseURL: "https://dev.thetrendlens.co.uk"
   },
   firebaseConfig: {
     apiKey: "AIzaSyAe4OWrwydmxI6pYtI0FpJv_ZXxtXUZ1-4",
