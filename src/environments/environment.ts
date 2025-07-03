507b973d7ddd8d229f7d2d7007e8e5f9e36016f7
James <jamesc554544@gmail.com>
2025-07-03 21:22:49 +0100

    Environment Vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,11 @@
+import {enableProdMode} from "@angular/core";
+
+enableProdMode();
+
 export const environment = {
-  production: false,
+  production: true,
   backend: {
-    baseURL: "https://dev.thetrendlens.co.uk"
+    baseURL: "https://api.thetrendlens.co.uk"
   },
   firebaseConfig: {
     apiKey: "AIzaSyAe4OWrwydmxI6pYtI0FpJv_ZXxtXUZ1-4",
@@ -12,6 +16,6 @@ export const environment = {
     appId: "1:917406287028:web:a451a3e375d88fab2443e1",
     measurementId: "G-Y6JM26S7CY"
   },
-  STRIPE_KEY: 'pk_test_51P6WfHKU9ihQF2dqWPo0TLuAvBvWaLgO9PAX7xQq2vNKD9DYVLTUlfVIlWI4lmYiTcGezzg4mzK8ZdE2GqXKladz00UfoscPUm',
-  STRIPE_PRICING_TABLE_ID: 'prctbl_1PAZAfKU9ihQF2dqlWu8TUOb',
+  STRIPE_KEY: 'pk_live_51P6WfHKU9ihQF2dql4dH6JePeqhckghCrmTxLmDyfCgL1sNpW71EZ4c2Q3YFJWWJ0bkinLTxGFktANVmRYLGB5zM00WsdPJ0bb',
+  STRIPE_PRICING_TABLE_ID: 'prctbl_1RgrKSKU9ihQF2dqIU8c4akd',
 }
